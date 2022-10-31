245129ce73aef082414ab6da288b086c6e11d349
lforellana <reckoner1991@gmail.com>
2022-10-30 21:45:04 -0600

    Servidor básico HTTP utilizando express

---
+++ README.md
@@ -0,0 +1,10 @@
+#Laboratorio 1
+Programacion IV
+Luis Fernando Orellana Diaz
+61911451
+Instrucciones:
+1. Clonar proyecto
+2. Instalar dependencias
+Npm install
+3. Ejectutar proyecto:
+Npm start
---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+res.send('Hello World')
+})
+
+app.listen(process.env.PORT, function(){
+console.console.log("El puerto esta corriendo el puerto TCP" + process.env.PORT)
+
+})
